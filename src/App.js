507a7ea3e4fe5763f2e11deead9c0a51ae05d8a9
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-17 21:12:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./Main";
 import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <main className="flex-grow">
           <Main />
         </main>
-        // <Footer />
+        
       </div>
     </Router>
   );
