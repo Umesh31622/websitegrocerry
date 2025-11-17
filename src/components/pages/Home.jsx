6a1bd3204d430a9eac1cf1cdcf3d5a679e6d071c
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-17 20:51:39 +0530

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -950,18 +950,8 @@ export default function Home() {
 
                 <div className="product-info">
                   <h4>{item.name}</h4>
-<p className="price">
-                          Today Rate: ₹
-                          {item.basePrice + (item.difference || 0)}
-                        </p>  
-                
-
-                  {/* TIME BASED PRICE */}
-                  <div className="product-price-box">
-                    {showRealPrice ? (
-                      <>  
-                        
-                       <p
+ 
+                 <p
                     className={`difference ${
                       item.difference >= 0 ? "positive" : "negative"
                     }`}
@@ -971,6 +961,16 @@ export default function Home() {
                       ? `+₹${item.difference}`
                       : `₹${item.difference}`}
                   </p>  
+
+                  {/* TIME BASED PRICE */}
+                  <div className="product-price-box">
+                    {showRealPrice ? (
+                      <>  
+                        <p className="price">
+                          Today Rate: ₹
+                          {item.basePrice + (item.difference || 0)}
+                        </p> 
+                      
                         <p className="store-status open">
                           Live Price (8AM–11PM)
                         </p> 
@@ -995,6 +995,7 @@ export default function Home() {
     </div>
   );
 }
+
 
 
 
